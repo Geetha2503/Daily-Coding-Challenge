98816d4958f18c7c14fa77f38f92144663822135
Geetha Janapareddi <82135489+Geetha2503@users.noreply.github.com>
2025-10-11 13:34:51 -0400

    Time: 668 ms (16.05%), Space: 0B (100%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,9 @@
+WITH cte AS (
+    SELECT *,
+           LEAD(num, 1) OVER (ORDER BY id) AS next_1,
+           LEAD(num, 2) OVER (ORDER BY id) AS next_2
+    FROM logs
+)
+SELECT DISTINCT num AS ConsecutiveNums
+FROM cte
+WHERE num = next_1 AND num = next_2;
